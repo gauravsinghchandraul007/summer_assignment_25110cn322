244c4492c075070e6ad5a3a9b5b69cec03b3f817
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2026-05-28 02:12:10 +0530

    Added Question 3 of Day 1

---
+++ Day_1/Question_3.java
@@ -0,0 +1,22 @@
+//Calculate factorial of given number :
+
+package Day_1;
+import java.util.Scanner;
+public class Question_3 {
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to find the factorial :");
+        int n = sc.nextInt();
+        int fact = 1;
+        for(int i= 1;i<=n;i++){
+            fact = fact*i;
+        }
+
+        System.out.println("The factorial of "+n+" is "+fact);
+        sc.close();
+
+
+    }
+    
+}
