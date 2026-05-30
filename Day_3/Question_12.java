9606959acade855bc2a7491282ea73e2201866cc
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2026-05-30 22:19:56 +0530

    Added Question 12 of Day 3

---
+++ Day_3/Question_12.java
@@ -0,0 +1,32 @@
+//Write a program to find LCM of two numbers.
+
+package Day_3;
+import java.util.Scanner;
+
+public class Question_12 {
+   
+ public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = sc.nextInt();
+
+        int x = a, y = b;
+
+        while (y != 0) {
+            int temp = y;
+            y = x % y; 
+            x = temp;
+        }
+
+        int gcd = x;
+        int lcm = (a * b) / gcd;
+
+        System.out.println("LCM = " + lcm);
+
+        sc.close();
+    }
+}
