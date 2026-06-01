7e2a0bd5af5a7f15fb84272150d3184b7c425629
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2026-06-01 22:05:37 +0530

    Added Question 18 of Day 5

---
+++ Day_5/Question_18.java
@@ -0,0 +1,25 @@
+//Write a program to Print factors of a number.
+
+package Day_5;
+
+import java.util.Scanner;
+
+public class Question_18 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.println("Factors of " + num + " are:");
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        sc.close();
+    }
+}
+
