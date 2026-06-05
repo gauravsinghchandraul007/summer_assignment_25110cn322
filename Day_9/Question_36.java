fad7f1688f7bf718f3c971e11a69636a6076489d
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2026-06-05 23:37:13 +0530

    Added Question 36 of Day 9

---
+++ Day_9/Question_36.java
@@ -0,0 +1,26 @@
+//Write a program to Print hollow square
+//pattern.
+//*****
+//* *
+//* *
+//* *
+//*****
+
+package Day_9;
+
+public class Question_36 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
