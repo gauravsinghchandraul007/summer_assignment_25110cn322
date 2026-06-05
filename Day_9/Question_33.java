01df9eb33d93f90e8e857b252553514b551f9100
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2026-06-05 23:00:09 +0530

    Added Question 33 of Day 9

---
+++ Day_9/Question_33.java
@@ -0,0 +1,14 @@
+package Day_9;
+
+public class Question_33 {
+      public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
