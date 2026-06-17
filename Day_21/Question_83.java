f116870cf4b6a2b9fddb2db1e002e685f4b542b8
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2026-06-17 21:14:46 +0530

    Added Question 83 of Day 21

---
+++ Day_21/Question_83.java
@@ -0,0 +1,33 @@
+//Write a program to Count vowels and consonants.
+
+package Day_21;
+
+import java.util.Scanner;
+
+public class Question_83 {
+   
+ public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine().toLowerCase();
+
+        int vowels = 0, consonants = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Number of vowels: " + vowels);
+        System.out.println("Number of consonants: " + consonants);
+        sc.close();
+    }
+}
