4ee32d7b6ef73096b430185206a377338e471517
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2026-06-21 20:13:46 +0530

    Added Question 85 of Day 22

---
+++ Day_22/Question_85.java
@@ -0,0 +1,30 @@
+//Write a program to Check palindrome string. 
+
+package Day_22;
+
+import java.util.Scanner;
+
+public class Question_85 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        // Check palindrome
+        if (str.equalsIgnoreCase(reversed)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        sc.close();
+    }
+}
