3de0db321c1d0c50dc99c1f814d0b814eb0363ed
Gaurav <gaurav@Gauravs-MacBook-Air.local>
2026-06-21 20:46:14 +0530

    Added Question 96 of Day 24

---
+++ Day_24/Question_96.java
@@ -0,0 +1,27 @@
+package Day_24;
+
+import java.util.Scanner;
+
+public class Question_96 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // Add character only if it is not already in result
+            if (result.indexOf(ch) == -1) {
+                result += ch;
+            }
+        }
+
+        System.out.println("String after removing duplicates: " + result);
+
+        sc.close();
+    }
+}
